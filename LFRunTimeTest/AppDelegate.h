8bc9d7716cdde1bf2495b102c7b68a1e289eb790
daoxiu <daoxiu@daoxiudemac-mini.local>
2016-07-15 09:49:35 +0800

    Initial Commit

---
+++ LFRunTimeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFRunTimeTest
+//
+//  Created by daoxiu on 16/7/15.
+//  Copyright © 2016年 aowokeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LFRunTimeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LFRunTimeTest
+//
+//  Created by daoxiu on 16/7/15.
+//  Copyright © 2016年 aowokeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
